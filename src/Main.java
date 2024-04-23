f8e73a4300b610b13377f706093e39daec78be05
Tanguri <jungwontwo2@naver.com>
2024-04-23 16:10:41 +0900

    완료

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,9 @@ public class Main {
     public static void main(String[] args) {
         ObjectMapper objectMapper = new ObjectMapper();
         AirQualityAlertSystem alertSystem = new AirQualityAlertSystem();
-//        DatabaseInitialization.createUser();
-//        DatabaseInitialization.createDatabase();
-//        DatabaseInitialization.createTable();
+        DatabaseInitialization.createUser();
+        DatabaseInitialization.createDatabase();
+        DatabaseInitialization.createTable();
         try {
             List<AirQualityData> dataList = objectMapper.readValue(new File("2023년3월_서울시_미세먼지.json"), new TypeReference<List<AirQualityData>>(){});
             // 데이터 리스트 사용
